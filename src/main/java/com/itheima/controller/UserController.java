a61f86902f009ac8307bdf3e0ef38a9fdd1c56e5
liyang <2384435151@qq.com>
2021-08-02 08:30:05 +0800

    第四次完成领导任务，查询功能
    Signed-off-by: liyang <2384435151@qq.com>

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.itheima.controller;
 
+import com.sun.org.apache.regexp.internal.RE;
+
 /**
  * @Auther:liyang
  * @Date:2021/8/1 - 08 -01 -16:22
  * @Description:com.itheima.controller
- * @Version:1.0
+ * @Version:4.0
  */
 public class UserController {
     public String login() {
         return "Login Success";
+    }
+
+    //    查询功能：
+    public String Query() {
+        return "Query Success";
     }
 }
